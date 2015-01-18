2106aab78d085d5fe6b3dbe3e097dfc06edbd7f8
dqwang <dequan.q.wang@nsn.com>
2015-01-18 10:15:58 +0800

    add update_app

--- update/update_all.sh
+++ update/update_all.sh
@@ -3,16 +3,12 @@ if [ -f nand1-2.tar.gz ];then
 fi
 	
 date > /home/jack/n32926/bsp/tftp/start/nand1-2/update_history
+cd /home/jack/n32926/bsp/tftp/start/nand1-2
+ls 
 
-cp /home/jack/n32926/bsp/tftp/start/nand1-2/app/bin/app /home/jack/n32926/bsp/tftp/update/nand1-2/app/bin/
-date > /home/jack/n32926/bsp/tftp/update/nand1-2/update_history
-#cd /home/jack/n32926/bsp/tftp/start/nand1-2
-cd /home/jack/n32926/bsp/tftp/update/nand1-2
-ls
 #don't modify the configuration on the directory : data/
 #tar -czvf nand1-2.tar.gz app  boa  data  update_history
-#tar -czvf nand1-2.tar.gz app boa update_history
-tar -czvf nand1-2.tar.gz app update_history
+tar -czvf nand1-2.tar.gz app boa update_history
 
 mv nand1-2.tar.gz /home/jack/n32926/bsp/itertk-001/update
 
